76f2ecf9d840d23e349c56d94aa58f8dec47e8c0
John Wood <john@johnpwood.net>
2012-06-30 10:34:28 -0500

    Private methods should start with an underscore, and use self:: to call them

--- lib/Proby/Notifier.php
+++ lib/Proby/Notifier.php
@@ -5,11 +5,11 @@ class Proby_Notifier
   public static function sendNotification($taskId, $type)
   {
     $requestor = new Proby_ApiRequestor();
-    $url = Proby_Notifier::notificationUrl($taskId, $type);
+    $url = self::_notificationUrl($taskId, $type);
     list($response, $apiKey) = $requestor->request('post', $url);
   }
 
-  private static function notificationUrl($taskId, $type)
+  private static function _notificationUrl($taskId, $type)
   {
     return "/api/v1/tasks/$taskId/$type.json";
   }
